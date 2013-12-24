dbb94bcdf7ee6be4f3b4384da2b1b93d989361cc
kaelzhang <i@kael.me>
2013-12-24 22:19:10 +0800

    first commit;

---
+++ test/npm-dir.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require('chai').expect;
+var dir = require('../');
+var npm = require('npm');
+
+describe("npm-dir", function(){
+    it(".dir", function(){
+        
+    });
+});
